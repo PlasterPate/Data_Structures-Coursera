99510da0a5dc7c0948920bcfabf6949c19d0729e
AliMohammad Movahedian <am.movahedian1998@hgmail.com>
2018-11-04 21:49:40 +0330

    ...

---
+++ A6/A6/EditDistance.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A6
+{
+    public class EditDistance: Processor
+    {
+        public EditDistance(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<string, string, long>)Solve);
+
+        public long Solve(string str1, string str2)
+        {
+            int[,] table = new int[str1.Length + 1, str2.Length + 1];
+            
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                table[i, 0] = i;
+            }
+            for (int j = 0; j < table.GetLength(1); j++)
+            {
+                table[0, j] = j;
+            }
+            for (int i = 1; i < table.GetLength(0); i++)
+            {
+                for (int j = 1; j < table.GetLength(1); j++)    
+                {
+                    if (str2[j - 1] == str1[i - 1])
+                        table[i, j] = table[i - 1, j - 1];
+                    else
+                        table[i, j] = Min(table[i - 1, j - 1],
+                                          table[i - 1, j],
+                                          table[i, j - 1] ) + 1;
+                }
+            }
+            return table[str1.Length, str2.Length];
+
+        }
+
+        private static int Min(params int[] numbers)
+        {
+            return numbers.Min();
+        }
+    }
+}
---
+++ A6/A6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoneyChange a = new MoneyChange("aaa");
+            a.Solve(34);
+        }
+    }
+}
