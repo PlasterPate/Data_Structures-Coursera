b9f297b8b46d6789b8ea8ef19d2cb22dc3e5d1a0
AliMohammad Movahedian <am.movahedian1998@hgmail.com>
2018-12-23 15:49:12 +0330

    testCommen

---
+++ A11/A11/BinaryTreeTraversals.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using TestCommon;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A11
+{
+    public class BinaryTreeTraversals : Processor
+    {
+        public BinaryTreeTraversals(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[][], long[][]>)Solve);
+
+        List<Node> tree;
+
+        public long[][] Solve(long[][] nodes)
+        {
+            tree = new List<Node>();
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                tree.Add(new Node(-1, -1, -1));
+                tree[i].Parent = -1;
+            }
+            foreach (var n in nodes)
+            {
+                tree.Add(new Node(n[0], n[1], n[2]));
+                tree[(int)n[1]].Parent = n[0];
+                tree[(int)n[2]].Parent = n[0];
+            }
+            long[][] result = new long[3][];
+            result[0] = InOrder();
+            return result;
+            
+        }
+
+        public long[] InOrder()
+        {
+            Stack<Node> inOrder = new Stack<Node>();
+            List<long> result = new List<long>();
+            inOrder.Push(tree[0]);
+            while (inOrder.Any())
+            {
+                var node = inOrder.Pop();
+                if (node.IsChecked)
+                {
+                    result.Add(node.Key);
+                }
+                else
+                {
+                    node.Check();
+                    inOrder.Push(tree[(int)node.Right]);
+                    inOrder.Push(node);
+                    inOrder.Push(tree[(int)node.Left]);
+                }
+            }
+            return result.ToArray();
+        }
+
+    }
+}
---
+++ A11/A11/Node.cs
@@ -0,0 +1,30 @@
+﻿namespace A11
+{
+    internal class Node
+    {
+        private long key;
+        private long parent;
+        private long left;
+        private long right;
+        private bool isChecked;
+
+        public Node(long key, long left, long right)
+        {
+            Key = key;
+            Left = left;
+            Right = right;
+            IsChecked = false;
+        }
+
+        public void Check()
+        {
+            IsChecked = true;
+        }
+
+        public long Key { get => key; set => key = value; }
+        public long Parent { get => parent; set => parent = value; }
+        public long Left { get => left; set => left = value; }
+        public long Right { get => right; set => right = value; }
+        public bool IsChecked { get => isChecked; set => isChecked = value; }
+    }
+}
